870384fe5ed997905e0a3c5b4a08c8876bb769ad
Ricky Nguyen <ricky.nguyen@gmail.com>
2019-04-15 17:05:02 -0700

    add tests

--- spec/helper.rb
+++ spec/helper.rb
@@ -7,12 +7,9 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
-require 'shoulda'
+require 'rspec'
+require 'webmock/rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'iap-validator'
-
-class Test::Unit::TestCase
-end
---
+++ spec/iap-validator_spec.rb
@@ -0,0 +1,38 @@
+require 'helper'
+
+RSpec.describe IAPValidator::IAPValidator do
+
+  it 'should validate' do
+    receipt_data = 'randomreceiptdata'
+    expected_response = { "cool" => 123 }
+    expected_response_str = MultiJson.dump(expected_response)
+
+    stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt').
+        with(body: { 'receipt-data' => receipt_data }).
+        and_return(status: 200, body: expected_response_str)
+
+    resp = IAPValidator::IAPValidator.validate(receipt_data, true)
+    expect(resp).to eq(expected_response)
+  end
+
+  it 'should validate with error callback' do
+    receipt_data = 'randomreceiptdata'
+    expected_response = { "cool" => 123 }
+    expected_response_str = MultiJson.dump(expected_response)
+
+    stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt').
+        with(body: { 'receipt-data' => receipt_data }).
+        and_return(status: 400, body: expected_response_str)
+
+    error = nil
+    resp = IAPValidator::IAPValidator.validate(receipt_data, true) do |error_resp|
+      error = error_resp
+    end
+
+    expect(resp).to be_nil
+    expect(error).to_not be_nil
+    expect(error.code).to eq(400)
+    expect(error.body).to eq(expected_response_str)
+  end
+
+end
