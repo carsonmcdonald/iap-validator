50737a8a67bb1327c09e9a13b32defaf2d0c1de2
Jonathan Suever <suever@gmail.com>
2019-04-24 11:09:13 -0400

    Fix some linting issues in specs

--- spec/iap-validator_spec.rb
+++ spec/iap-validator_spec.rb
@@ -1,15 +1,14 @@
 require 'helper'
 
 RSpec.describe IAPValidator::IAPValidator do
-
   it 'should validate' do
     receipt_data = 'randomreceiptdata'
-    expected_response = { "cool" => 123 }
+    expected_response = { 'cool' => 123 }
     expected_response_str = MultiJson.dump(expected_response)
 
-    stub = stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt').
-        with(body: { 'receipt-data' => receipt_data }).
-        and_return(status: 200, body: expected_response_str)
+    stub = stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt')
+           .with(body: { 'receipt-data' => receipt_data })
+           .and_return(status: 200, body: expected_response_str)
 
     resp = IAPValidator::IAPValidator.validate(receipt_data, true)
 
@@ -19,17 +18,16 @@ RSpec.describe IAPValidator::IAPValidator do
 
   it 'should validate with error' do
     receipt_data = 'randomreceiptdata'
-    expected_response = { "cool" => 123 }
+    expected_response = { 'cool' => 123 }
     expected_response_str = MultiJson.dump(expected_response)
 
-    stub = stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt').
-        with(body: { 'receipt-data' => receipt_data }).
-        and_return(status: 400, body: expected_response_str)
+    stub = stub_request(:post, IAPValidator::IAPValidator::SANDBOX_URL + '/verifyReceipt')
+           .with(body: { 'receipt-data' => receipt_data })
+           .and_return(status: 400, body: expected_response_str)
 
     resp = IAPValidator::IAPValidator.validate(receipt_data, true)
 
     expect(stub).to have_been_requested
     expect(resp).to be_nil
   end
-
 end
